9a6418a6c8fcfbe65327eae8a7338a61a6bd02a5
jeffrey6109 <striker.jk95@gmail.com>
2021-01-10 12:23:33 +0800

    update 2.1

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="card ">
-                <div class="card-header text-center">Produce List</div>
+                <div class="card-header text-center">Product List</div>
 
                 <div class="card-body">
 
