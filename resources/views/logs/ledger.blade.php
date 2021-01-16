67d6aa9c409a55e0b4ed2f7e605fb69e69605067
jeffrey6109 <striker.jk95@gmail.com>
2021-01-16 16:05:14 +0800

    Added my project

--- resources/views/logs/ledger.blade.php
+++ resources/views/logs/ledger.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="card">
-                <div class="card-header">Logs</div>
+                <div class="card-header">Ledger</div>
 
                 <div class="card-body">
 
